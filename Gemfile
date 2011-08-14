425912371989a5d717503f5e64587386205964dc
Stanley Kamithi <skamithi@gmail.com>
2011-08-14 17:16:08 -0400

    base changes for me.

--- Gemfile
+++ Gemfile
@@ -1,76 +1,76 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.6'
+# Core Rails version
+gem 'rails', '3.0.9'
 
-# Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
+# generator helper gem
+gem 'rails3-generators'
 
-gem 'mysql2'
-# gem 'sqlite3-ruby', :require => 'sqlite3'
-# gem 'moneta'
+#javascript related
+gem 'therubyracer'
+gem 'barista'
+gem 'jquery-rails'
 
-gem 'yajl-ruby', :require => 'yajl'
-gem 'nokogiri'
+# rails server
+gem 'thin'
+
+#DB Protocol
+gem 'mysql2', '0.2.6'
+
+#Basic search help engine
+gem 'meta-where'
+
+#Full Text searching engine
+gem 'sunspot'
+
+
+# HTML Parsing tools
+#gem 'yajl-ruby', :require => 'yajl'
+#gem 'nokogiri'
 
 # Paginator
-# gem "kaminari"
-# gem 'will_paginate', '3.0.pre2'
+#gem "kaminari"
+
 
 # File Uploads
 # gem 'paperclip'
-# gem 'carrierwave'
 
-# gem 'devise', '~> 1.1.7'
-# gem "cancan"
-# gem "simple_form"
-# gem 'fastercsv' # for Ruby 1.8.7+
-# gem 'prawn'
-# gem "haml"
-# gem "haml-rails"
-# gem 'validates_timeliness', '~> 3.0.5'
-# gem 'acts-as-taggable-on'
- 
-# gem 'hoptoad_notifier'
-# gem 'newrelic_rpm'
 
-gem 'SystemTimer' if RUBY_VERSION =~ /^1.8/
+# Authentication
+#gem 'devise'
+
+# Authorization
+#gem "cancan"
+
+# Form Helpers
+gem "simple_form"
+
+# Replaces ERB
+gem "haml"
+gem "haml-rails"
+
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
 
 group :test, :development do
-  gem "rspec", "~> 2.0"
-  gem "rspec-rails", "~> 2.0"
-  gem "factory_girl_rails"
-  gem "shoulda-matchers"
-  gem "rcov"
-  gem "delorean"
-  gem "watchr"
-  # gem "capybara"
-  # gem 'yard'
-  # gem "bluecloth"
+  gem 'ruby-debug19', :require => 'ruby-debug'
+  gem 'w3c_validators'
+  gem 'cucumber'
+  gem 'cucumber-rails'
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'shoulda-matchers'
+  gem 'rb-inotify'
+  gem 'guard-rspec'
+  gem 'guard-cucumber'
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'pickle'
+  gem 'factory_girl_rails'
+  gem 'launchy'
+  gem 'escape_utils'
+  gem 'guard-bundler'
 end
 
-# State machine
-# gem 'aasm'
-# gem 'state_machine'
-
-# HTTP client
-# gem 'typhoeus'
-# gem 'rest-client', :require => 'restclient'
-
-# Memcached client 
-# gem "dalli"
-# gem "memcache-client", :require => "memcache"
-
-# Deployment tool
-# gem 'capistrano'
-# gem 'whiskey_disk'
-
-# Debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
-# gem 'ruby-debug'
-# gem 'ruby-debug19'
-
-# Background Processing
-# gem 'delayed_job'
-# gem 'resque'
-
-# Full-text search engine
-# gem 'thinking-sphinx', '~> 2.0.2', :require => 'thinking_sphinx'
--- rails3.rb
+++ rails3.rb
@@ -1,3 +1,6 @@
+# execute rvm
+run "rvm use 1.9.2 --rvmrc"
+
 # remove files
 run "rm README"
 run "rm public/index.html"
@@ -11,38 +14,61 @@ file 'Gemfile', File.read("#{File.dirname(rails_template)}/Gemfile")
 # bundle install
 run "bundle install"
 
+
+application  <<-GENERATORS
+config.generators do |g|
+  g.template_engine :haml
+  g.test_framework  :rspec, :fixture => true, :views => false
+  g.integration_tool :rspec, :fixture => true, :views => true
+  g.fixture_replacement :factory_girl, :dir => "spec/support/factories"
+end
+GENERATORS
+
 # generate rspec
 generate "rspec:install"
+
+#generate cucubmer
+generate "cucumber:install --capybara --rspec"
+
+#generate pickle
+generate "pickle --path --email"
+
+#generate simple form files
+generate "simple_form:install"
+
+#generate coffeescript files
+generate "barista:install"
+# add app/coffescripts directory
 
 # copy files
 file 'script/watchr.rb', File.read("#{File.dirname(rails_template)}/watchr.rb")
 file 'lib/tasks/dev.rake', File.read("#{File.dirname(rails_template)}/dev.rake")
 
-# remove active_resource and test_unit
-gsub_file 'config/application.rb', /require 'rails\/all'/, <<-CODE
-  require 'rails'
-  require 'active_record/railtie'
-  require 'action_controller/railtie'
-  require 'action_mailer/railtie'
-CODE
-
 # install jquery
-run "curl -L http://code.jquery.com/jquery.min.js > public/javascripts/jquery.js"
-run "curl -L http://github.com/rails/jquery-ujs/raw/master/src/rails.js > public/javascripts/rails.js"
 
-gsub_file 'config/application.rb', /(config.action_view.javascript_expansions.*)/, 
+
+gsub_file 'config/application.rb', /(config.action_view.javascript_expansions.*)/,
                                    "config.action_view.javascript_expansions[:defaults] = %w(jquery rails)"
 
 # add time format
-environment 'Time::DATE_FORMATS.merge!(:default => "%Y/%m/%d %I:%M %p", :ymd => "%Y/%m/%d")'
+#environment 'Time::DATE_FORMATS.merge!(:default => "%Y/%m/%d %I:%M %p", :ymd => "%Y/%m/%d")'
 
 # .gitignore
 append_file '.gitignore', <<-CODE
+bin/
+.rvm*
+.metadata
+.project
+.idea/
+show/
+db/schema.rb
+Gemfile.lock
+.bundle
+db/*.sqlite3
+log/
+tmp/
 config/database.yml
-Thumbs.db
-.DS_Store
-tmp/*
-coverage/*
+test/
 CODE
 
 # keep tmp and log
@@ -50,8 +76,9 @@ run "touch tmp/.gitkeep"
 run "touch log/.gitkeep"
 
 # git commit
-git :init
-git :add => '.'
-git :add => 'tmp/.gitkeep -f'
-git :add => 'log/.gitkeep -f'
-git :commit => "-a -m 'initial commit'"
+#git :init
+#git :add => '.'
+#git :add => 'tmp/.gitkeep -f'
+#git :add => 'log/.gitkeep -f'
+#git :commit => "-a -m 'initial commit'"
+
